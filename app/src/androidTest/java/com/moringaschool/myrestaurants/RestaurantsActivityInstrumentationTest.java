02232281e25972321fce2bc262cc8f232715e3c3
Blackscure <Wekesabuyahi@gmail.com>
2020-06-09 13:25:35 +0300

    app folder

---
+++ app/src/androidTest/java/com/moringaschool/myrestaurants/RestaurantsActivityInstrumentationTest.java
@@ -0,0 +1,45 @@
+package com.moringaschool.myrestaurants;
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.moringaschool.myrestaurants.ui.RestaurantListActivity;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.core.IsNot.not;
+
+@RunWith(AndroidJUnit4.class)
+public class RestaurantsActivityInstrumentationTest {
+    @Rule
+    public ActivityTestRule<RestaurantListActivity> activityTestRule =
+            new ActivityTestRule<>(RestaurantListActivity.class);
+
+    @Test
+    public void listItemClickDisplaysToastWithCorrectRestaurant() {
+        View activityDecorView = activityTestRule.getActivity().getWindow().getDecorView();
+        onView(withId(R.id.listView)).check(matches(isDisplayed()));
+        String restaurantName = "Mi Mero Mole";
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .perform(click());
+        onView(withText(restaurantName)).inRoot(withDecorView(not(activityDecorView)));
+    }
+}
+
+
